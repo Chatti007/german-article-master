8763c22ea417953898224687e2717bc1771110bc
Chatti007 <mohamedchattifna@gmail.com>
2025-11-16 18:09:28 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// config.example.js - آمن للرفع على GitHub
+export const firebaseConfig = {
+  apiKey: "replace_with_your_real_api_key",
+  authDomain: "your-project.firebaseapp.com",
+  databaseURL: "https://your-project.firebasedatabase.app",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
+};

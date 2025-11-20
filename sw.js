5ad7a5302445dccc2375ea7c92d61ab1837323d0
Chatti007 <mohamedchattifna@gmail.com>
2025-11-21 00:56:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,22 @@
-const CACHE_NAME = 'german-app-v1';
+// sw.js - Service Worker بسيط
+const CACHE_NAME = 'german-master-v1';
 const urlsToCache = [
   '/german-article-master/',
   '/german-article-master/index.html',
-  '/german-article-master/german_words.json'
+  '/german-article-master/manifest.json',
+  '/german-article-master/AppImages/Android/android192-192.png'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+      .then(response => response || fetch(event.request))
   );
 });
